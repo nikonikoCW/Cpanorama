7961df4c2472434664331f80c48e43fd5d56b2c7
chenwei <84675738@qq.com>
2020-12-03 10:54:38 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app,html,body{
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <div id="panorama"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+    debugger
+    pannellum.viewer('panorama', {
+        "type":"multires",
+        "autoLoad": true, 
+        "autoRotate":"3",
+        "autoRotateInactivityDelay":'10000',
+        "multiRes": {
+            "path": "/%l/%s%y_%x",
+            "fallbackPath": "/fallback/%s",
+            "basePath":'../../static/pano/output',
+            "extension": "jpg",
+            "tileResolution": 512,
+            "maxLevel": 4,
+            "cubeResolution": 2544
+        }
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#panorama,.hello {
+            width: 100%;
+            height: 100%;
+        }
+</style>
